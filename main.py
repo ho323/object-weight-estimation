b2f5e3697d7769cd85b9d044297d29ec4fcb7de0
ho323 <8536048@gmail.com>
2025-05-13 14:54:17 +0900

    1.5

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import numpy as np
 import open3d as o3d
 import torch
 from PIL import Image
-from model.yolo import load_yolo_model, run_yolo_on_image
-from model.mtr_clf import init_model, materials_classification
+from model.yolo import load_yolo_model, run_yolo_inference
 from depth_anything_v2.dpt import DepthAnythingV2
 from time import time
 
@@ -19,15 +18,10 @@ def load_depth_model(device):
     de_model.eval()
     return de_model
 
-def estimate_depth(raw_img, model):
+def estimate_depth(model, raw_img):
     with torch.no_grad():
         return model.infer_image(raw_img)
 
-# YOLO 모델을 위한 함수
-def load_yolo_model_and_run(raw_img, model_path, conf_threshold=0.6):
-    model = load_yolo_model(model_path)
-    results = run_yolo_on_image(raw_img, model, conf_threshold)
-    return results
 
 def extract_bounding_boxes_and_crops(results):
     result = results[0]  # Assuming results are in a list
@@ -50,20 +44,28 @@ def extract_bounding_boxes_and_crops(results):
 # Depth to PointCloud 및 부피 계산 함수
 def depth_to_pointcloud(depth_map, bbox):
     x_min, y_min, x_max, y_max = bbox
-    cx = (x_min + x_max) // 2
-    cy = (y_min + y_max) // 2
+    fx = cfg.CAMERA_PARAMETER['fx']
+    fy = cfg.CAMERA_PARAMETER['fy']
+    cx = (x_min + x_max) / 2
+    cy = (y_min + y_max) / 2
+
+    print(cfg.CAMERA_PARAMETER)
 
     depth_region = depth_map[y_min:y_max, x_min:x_max]
+    h, w = depth_region.shape
+    print(h, w)
 
-    u, v = np.meshgrid(np.arange(x_min, x_max), np.arange(y_min, y_max))
+    u = np.linspace(x_min, x_max - 1, w)
+    v = np.linspace(y_min, y_max - 1, h)
+    uu, vv = np.meshgrid(u, v)
 
     # 3D 좌표 계산 (벡터화된 방식)
-    x = (u - cx) * depth_region / cfg.FOCAL_LENGTH['fx']
-    y = (v - cy) * depth_region / cfg.FOCAL_LENGTH['fy']
+    x = (uu - cx) * depth_region / fx
+    y = (vv - cy) * depth_region / fy
     z = depth_region
 
-    points = np.vstack((x.flatten(), y.flatten(), z.flatten())).T
-    return points, x, y, z  # x, y, z를 반환
+    points = np.stack((x, y, z), axis=-1).reshape(-1, 3)
+    return points
 
 def calc_volume(points):
     pcd = o3d.geometry.PointCloud()
@@ -76,167 +78,86 @@ def calc_volume(points):
     voxel_volume = voxel_size ** 3
     volume = num_voxels * voxel_volume
 
-    return volume
+    # 물체의 크기 계산
+    points_array = np.asarray(pcd.points)
+    min_bound = np.min(points_array, axis=0)
+    max_bound = np.max(points_array, axis=0)
+    dimensions = max_bound - min_bound  # [width, height, depth]
+
+    return volume, dimensions
 
 # 물질 분류 및 무게 계산 함수
-def material_classification_and_weight(cropped_images, boxes, class_ids, depth_map, mtr_model, mtr_classes, result):
-    depth_infos = []
-    for i, (crop_img, box, cls_id) in enumerate(zip(cropped_images, boxes, class_ids)):
-        # 3D 좌표 및 가로, 세로, 깊이 정보 얻기
-        points, x, y, z = depth_to_pointcloud(depth_map, box)
+def weight_estimation(cropped_images, boxes, class_ids, depth_map, result):
+    out = []
+    for i, (img, box, cls_id) in enumerate(zip(cropped_images, boxes, class_ids)):
+        points = depth_to_pointcloud(depth_map, box)
+        volume, dimensions = calc_volume(points)
 
-        # # 실제 측정한 가로, 세로, 깊이
-        # width = np.max(x) - np.min(x)
-        # height = np.max(y) - np.min(y)
-        # depth = np.mean(z)
-
-        # 부피 계산
-        volume = calc_volume(points)
-
-        # 클래스 이름 가져오기
-        class_name = result.names[cls_id]
-
-        # 평균 크기 및 무게 가져오기
-        if class_name in cfg.COCO_CLASS_INFO:
-            avg_info = cfg.COCO_CLASS_INFO[class_name]
-
-            avg_width = avg_info['width']
-            avg_height = avg_info['height']
-            avg_depth = avg_info['depth']
-            avg_weight = avg_info['weight']
-
-            # 부피 비율로 보정 (3축 모두)
-            average_volume = avg_width * avg_height * avg_depth
-
-            if average_volume > 0:
-                volume_ratio = volume / average_volume
-                weight = avg_weight * volume_ratio
-            else:
-                weight = avg_weight  # 혹시 평균 부피가 0이면 그냥 평균 무게 사용
+        cname = result.names[cls_id]
+        info  = cfg.COCO_CLASS_INFO.get(cname)
+        if info:
+            avg_vol = info['width'] * info['height'] * info['depth']
+            weight  = info['weight'] * (volume / avg_vol) if avg_vol > 0 else info['weight']
         else:
-            # 클래스 정보가 없으면 조정 없이 0
             weight = 0
 
-        depth_infos.append({
-            "id": i,
-            "class": class_name,
-            "volume": volume,
-            # "width": width,
-            # "height": height,
-            # "depth": depth,
+        out.append({
+            "id": i, 
+            "class": cname, 
+            "volume": volume, 
             "weight": weight,
+            "width": dimensions[0],
+            "height": dimensions[1], 
+            "depth": dimensions[2]
         })
-
-    return depth_infos
-
-def calculate_weight_for_materials(depth_infos):
-    for info in depth_infos:
-        material = info['material']
-        weight = cfg.DENSITIES[material] * info['volume']
-        print(f"[{info['id']}] {info['class']}'s Weight: {weight/1000:.2f}kg")
+    return out
 
 # 전체 파이프라인 함수
 def process_image(image_path, output_path, save_image=False):
     raw_img = cv2.imread(image_path)
     rgb_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2RGB)  # 이미지 변환 (BGR -> RGB)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Current device:", cfg.DEVICE)
 
-    print("Depth Estimation running...")
-    de_model = load_depth_model(device)
-    depth_map = estimate_depth(rgb_img, de_model)
+    print("Models Loading...")
+    de_model = load_depth_model(cfg.DEVICE)
+    od_model = load_yolo_model(cfg.OD_MODEL_PATH, cfg.DEVICE)
     
-    print("YOLOv8 running...")
-    results = load_yolo_model_and_run(rgb_img, cfg.OD_MODEL_PATH, conf_threshold=0.6)
+    print("Depth Estimation running...")
+    depth_map = estimate_depth(de_model, rgb_img)
 
+    print("YOLOv8 running...")
+    results = run_yolo_inference(od_model, raw_img, conf_threshold=0.7)
     cropped_images, boxes, class_ids, class_names = extract_bounding_boxes_and_crops(results)
 
-    mtr_model, mtr_classes = init_model()
-    depth_infos = material_classification_and_weight(cropped_images, boxes, class_ids, depth_map, mtr_model, mtr_classes, results[0])
+    print("Weight Estimation running...")
+    stime = time()
+    depth_infos = weight_estimation(cropped_images, boxes, class_ids, depth_map, results[0])
+    print(f"Total processing time is {(time() - stime):.2f}s")
 
     # BBox 옆에 라벨 추가
     for i, (box, cls_id, class_name) in enumerate(zip(boxes, class_ids, class_names)):
         x1, y1, x2, y2 = box
-        volume = depth_infos[i]['volume']
         weight = depth_infos[i]['weight']
-        # width = depth_infos[i]['width']
-        # height = depth_infos[i]['height']
-        # depth = depth_infos[i]['depth']
+        pushable = "Pushable" if weight < 3.5 else "Unpushable"
         
-        label = f"{class_name}, Vol: {volume:.1f}cm^3, Weight: {weight:.1f}kg"
-        # dimension_info = f"W: {width:.1f}, H: {height:.1f}cm, D: {depth:.1f}cm"
+        label = f"{class_name}, Weight: {weight:.1f}kg, {pushable}"
         print(label)
         
         # BBox 그리기
-        cv2.rectangle(rgb_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        if pushable == "Pushable":
+            cv2.rectangle(rgb_img, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        else:
+            cv2.rectangle(rgb_img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
         # 라벨 추가
-        cv2.putText(rgb_img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
-        # cv2.putText(rgb_img, dimension_info, (x1, y2 + 15), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+        cv2.putText(rgb_img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (125, 255, 125), 3)
 
     if save_image:
         # RGB 이미지를 저장 (BGR로 변환하여 저장)
         output_bgr = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2BGR)  # 다시 BGR로 변환
         cv2.imwrite(output_path, output_bgr)
         print("Image saved as output.png")
-
-
-# 실시간 영상 또는 동영상 처리 함수
-def process_video(video_source=0):
-    cap = cv2.VideoCapture(video_source)
-    if not cap.isOpened():
-        print("❌ Cannot open camera or video.")
-        return
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    de_model = load_depth_model(device)
-    mtr_model, mtr_classes = init_model()
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        raw_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        print("Depth Estimation running...")
-        depth_map = estimate_depth(raw_img, de_model)
-
-        print("YOLOv8 running...")
-        results = load_yolo_model_and_run(frame, cfg.OD_MODEL_PATH, conf_threshold=0.6)
-
-        cropped_images, boxes, class_ids, class_names = extract_bounding_boxes_and_crops(results)
-
-        # 물질 분류 및 무게 계산
-        depth_infos = material_classification_and_weight(cropped_images, boxes, class_ids, depth_map, mtr_model, mtr_classes, results[0])
-
-        # 물체별 정보 및 BBox 그리기
-        for i, (box, cls_id) in enumerate(zip(boxes, class_ids)):
-            x1, y1, x2, y2 = box
-            material = depth_infos[i]['material']
-            volume = depth_infos[i]['volume']
-            weight = cfg.DENSITIES[material] * volume
-            width = depth_infos[i]['width']
-            height = depth_infos[i]['height']
-            depth = depth_infos[i]['depth']
-            
-            label = f"{results[0].names[cls_id]} {material} Vol: {volume:.2f}cm³ W: {weight/1000:.2f}kg"
-            dimension_info = f"WxH: {width:.2f}x{height:.2f}cm D: {depth:.2f}cm"
-            
-            # BBox 그리기
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            # 라벨 추가
-            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
-            cv2.putText(frame, dimension_info, (x1, y2 + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
-        # 실시간 화면에 출력
-        cv2.imshow('Real-Time Object Detection', frame)
-
-        # 'q' 키를 눌러 종료
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
 
 
 def parse_args():
@@ -253,5 +174,5 @@ if __name__ == "__main__":
 
     if args.image_path:
         process_image(args.image_path, output_path=args.output_path, save_image=args.save_image)
-    else:
-        process_video(args.video_source)
+    # else:
+    #     process_video(args.video_source)
